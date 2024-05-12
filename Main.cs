96987d5cb1aacba5e87cd67e6afaf1655f442344
Nick Fox <66554932+NickFox007@users.noreply.github.com>
2024-05-12 15:01:39 +0300

    0.4.1
    Fix exceptions when player exited while opened menu

---
+++ Main.cs
@@ -0,0 +1,57 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Core.Capabilities;
+using CounterStrikeSharp.API.Modules.Commands;
+using CounterStrikeSharp.API.Modules.Menu;
+using PlayerSettings;
+
+
+namespace MenuManager;
+public class MenuManagerCore : BasePlugin
+{
+    public override string ModuleName => "MenuManager [Core]";
+    public override string ModuleVersion => "0.4.1";
+    public override string ModuleAuthor => "Nick Fox";
+    public override string ModuleDescription => "All menus interacts in one core";
+
+    private IMenuApi? _api;
+    private ISettingsApi? _settings;
+    private readonly PluginCapability<IMenuApi?> _pluginCapability = new("menu:nfcore");
+    private readonly PluginCapability<ISettingsApi?> _settingsCapability = new("settings:nfcore");
+    public override void Load(bool hotReload)
+    {
+        _api = new CMenuApi(this);
+        Capabilities.RegisterPluginCapability(_pluginCapability, () => _api);
+
+        Control.SetPlugin(this);
+        RegisterListener<Listeners.OnTick>(() => { Control.OnPluginTick(); });
+    }
+
+    public override void OnAllPluginsLoaded(bool hotReload)
+    {
+        _settings = _settingsCapability.Get();
+        if (_settings == null) Console.WriteLine("PlayerSettings core not found...");
+        Misc.SetSettingApi(_settings);
+    }
+
+    public override void Unload(bool hotReload){
+        Control.Clear();
+    }
+
+    [ConsoleCommand("css_menus", "Choose menu type")]
+    public void OnCommand(CCSPlayerController? player, CommandInfo command)
+    {
+        if (player != null)
+        {
+            var menu = _api.NewMenu("Выбор меню");
+            menu.AddMenuOption("Консольное", (player, option) => { Misc.SelectPlayerMenu(player, MenuType.ConsoleMenu); });
+            menu.AddMenuOption("Чат-меню", (player, option) => { Misc.SelectPlayerMenu(player, MenuType.ChatMenu); });
+            menu.AddMenuOption("Стандартное (центр)", (player, option) => { Misc.SelectPlayerMenu(player, MenuType.CenterMenu); });
+            menu.AddMenuOption("Управляемое (центр)", (player, option) => { Misc.SelectPlayerMenu(player, MenuType.ButtonMenu); });
+            menu.Open(player);
+
+        }
+
+    }
+}
---
+++ Misc.cs
@@ -0,0 +1,52 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PlayerSettings;
+
+
+namespace MenuManager
+{
+    internal static class Misc
+    {
+
+        private static ISettingsApi? settings;
+        public static void SetSettingApi(ISettingsApi _settings)
+        {
+            settings = _settings;
+        }
+        public static List<CCSPlayerController> GetValidPlayers()
+        {
+            var players = new List<CCSPlayerController>();
+            foreach (var player in Utilities.GetPlayers())
+            {
+                if (player != null && player.IsValid && !player.IsBot && !player.IsHLTV && player.Connected == PlayerConnectedState.PlayerConnected)
+                    players.Add(player);
+            }
+
+            return players;
+        }
+
+        public static MenuType GetCurrentPlayerMenu(CCSPlayerController player)
+        {         
+            var res = settings.GetPlayerSettingsValue(player, "menutype", "ButtonMenu");
+            return (MenuType)Enum.Parse(typeof(MenuType), res);
+        }
+
+        public static void SelectPlayerMenu(CCSPlayerController player, MenuType type)
+        {
+            settings.SetPlayerSettingsValue(player, "menutype", Enum.GetName(type.GetType(), type));
+        }
+
+        public static bool IsValidPlayer(CCSPlayerController player)
+        {
+            if (player.IsValid && player.Connected == PlayerConnectedState.PlayerConnected && !player.IsBot) return true;
+            else return false;
+        }
+    }
+
+    
+}
